907a8343a1821a3313143f1bfb210f5edc3c2de3
fabio lima <fabiolima39@gmail.com>
2019-06-14 21:08:15 -0300

    Comments

--- PuzzleC/Operators1.c
+++ PuzzleC/Operators1.c
@@ -4,15 +4,19 @@ int main() {
 	int x;
 	int z;
 	
+	//Operators 1.1
 	// first 4*5 then -3 or -6 
 	x = -3 + 4 * 5 - 6;
 	z = (-3 + (4 * 5)) - 6; printf ("to prove results: %d %d\n", x, z);
+	//Operators 1.2
 	// first 4 % 5 then 3 or -6 
 	x = 3 + 4 % 5 - 6;
 	z = (3 + (4 % 5) - 6); printf("to prove results: %d %d \n", x, z);
+	//Operators 1.3
 	// first -3 * 4  then -6 / 5 after execute results with -
 	x = - 3 * 4 % - 6 / 5; 
 	z = (-3 * 4) % (-6 / 5); printf("to prove results: %d %d\n", x, z);
+	//Operators 1.4
 	// first (7 + 6) after result % 5 then /2
 	x = (7 + 6) % 5 / 2; 
 	z = ((7+6) % 5) / 2; printf("to prove results: %d %d\n", x, z);
