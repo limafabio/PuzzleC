d5933d419fbff1eaf0f9e494a51e511cf54c17ad
fabio lima <fabiolima39@gmail.com>
2019-06-30 22:12:10 -0300

    operators 3

--- PuzzleC/Operators3.c
+++ PuzzleC/Operators3.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
-#define PRINT(int) printf("%d\n",int)
-#define PRINTRESUL(a,b) printf("compare result %d %d\n",a,b)
+#define PRINT(a,b,c,d) printf("Operators %d.%d: %d = %d \n", a,b,c,d);
 
 int main()
 {
@@ -9,33 +8,20 @@ int main()
 	x = 2; y = 1; z = 0;
 	X = 2; Y = 1; Z = 0;
 
-	//Operators 3.1
-	x = x && y || z ; PRINT(x);
-	X = ((X && Y) || Z); PRINTRESUL(x,X);
+	PRINT(3,1, x = x && y || z, X = ((X && Y) || Z));
 		
-	//Operators 3.2
-	int res;
-	PRINT(x || y && z);
-	res = (X || ( Y  && Z));
-	PRINTRESUL( (x || y && z), res);
+	PRINT(3,2,(x || y && z),(X || ( Y  && Z)));
 
-	//Operators 3.3
 	x = y = 1;
-	z = x ++ -1; PRINT(x); PRINT(z);
+	z = x ++ -1;
 	Z = 0; X = Y = 1;
 	Z = (X++) -1;
-	PRINTRESUL(z,Z);
-	PRINTRESUL(x,X);
+	PRINT(3,3,z,Z);
+	PRINT(3,3,x,X);
 
-	//Operators 3.4
-	z += -x ++ + ++ y; PRINT(x); PRINT(z);
-	Z += -(X++) + (+ 1 + Y);
-	PRINTRESUL(z,Z);
-	PRINTRESUL(x,X);
+	PRINT(3,4, z += -x ++ + ++ y, Z += -(X++) + (+ 1 + Y));
+	PRINT(3,4,x,X);
 
-	//Operators 3.5
-	z = x / ++ x; PRINT(z);
-	Z = (X / (++X));
-	PRINTRESUL(z,Z);
+	PRINT(3,5, z = x / ++ x , Z = (X / (++X)) );
 
 }
