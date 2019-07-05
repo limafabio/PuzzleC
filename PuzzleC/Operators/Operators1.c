3c244aaa5c8fad6e524014877ff207b3d769b67a
fabio lima <fabiolima39@gmail.com>
2019-07-05 20:31:45 -0300

    arrange files

--- PuzzleC/Operators/Operators1.c
+++ PuzzleC/Operators/Operators1.c
@@ -1,14 +1,10 @@
 #include<stdio.h>
-<<<<<<< HEAD
 #define PRINT(a,b,c,d) printf("Operators %d.%d:  %d = %d \n",a,b,c,d);
-=======
->>>>>>> operators5
 
 int main() {
 	int x;
 	int z;
 	
-<<<<<<< HEAD
 	x = -3 + 4 * 5 - 6;
 	z = (-3 + (4 * 5)) - 6;
        	PRINT(1,1,x,z);
@@ -24,18 +20,4 @@ int main() {
 	x = (7 + 6) % 5 / 2; 
 	z = ((7+6) % 5) / 2;
 	PRINT(1,4,x,z);
-=======
-	// first 4*5 then -3 or -6 
-	x = -3 + 4 * 5 - 6;
-	z = (-3 + (4 * 5)) - 6; printf ("to prove results: %d %d\n", x, z);
-	// first 4 % 5 then 3 or -6 
-	x = 3 + 4 % 5 - 6;
-	z = (3 + (4 % 5) - 6); printf("to prove results: %d %d \n", x, z);
-	// first -3 * 4  then -6 / 5 after execute results with -
-	x = - 3 * 4 % - 6 / 5; 
-	z = (-3 * 4) % (-6 / 5); printf("to prove results: %d %d\n", x, z);
-	// first (7 + 6) after result % 5 then /2
-	x = (7 + 6) % 5 / 2; 
-	z = ((7+6) % 5) / 2; printf("to prove results: %d %d\n", x, z);
->>>>>>> operators5
 }
